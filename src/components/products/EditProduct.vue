bf4bd0acdd36e5628e1378d6c0bab03bca07670f
Mark de Haan <mark.dehaan@inholland.nl>
2022-11-27 18:50:46 +0100

    first commit

---
+++ src/components/products/EditProduct.vue
@@ -0,0 +1,79 @@
+<template>
+  <section>
+    <div class="container">
+      <form ref="form">
+        <h2 class="mt-3 mt-lg-5">Edit a product</h2>
+        <h5 class="mb-4"></h5>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">Name</span>
+          <input type="text" class="form-control" v-model="product.name" />
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">Price</span>
+          <input type="number" class="form-control" v-model="product.price" />
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">Description</span>
+          <textarea
+            class="form-control"
+            v-model="product.description"
+          ></textarea>
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">Image URL</span>
+          <input type="text" class="form-control" v-model="product.image" />
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">Category</span>
+          <select class="form-select">
+            <option value="testoptionvalue">test option       
+            </option>
+          </select>
+        </div>
+
+        <div class="input-group mt-4">
+          <button type="button" class="btn btn-primary">
+            Save changes
+          </button>
+          <button
+            type="button"
+            class="btn btn-danger"
+            @click="this.$router.push('/products')"
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "EditProduct",
+  props: {
+    id: Number,
+  },
+  data() {
+    return {
+      product: {
+        id: 0,
+        name: "",
+        price: 0.0,
+        description: "",
+        image: "",
+        category_id: 0,
+      },
+      categories: [],
+    };
+  }  
+};
+</script>
+
+<style>
+</style>
