c9a447556a6bd292bdbeaa1d3072e214f2b6554e
robytrierweiler <roby.trierweiler@energie-pass.lu>
2023-05-24 03:30:53 +0200

    another

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -8,20 +8,10 @@
             <br>
             <p>Account number: 123456789</p>
             <p>Account holder: John Doe</p>
-            <p>Account balance(total): 1000</p>
+            <p>Account balance(total): 2000</p>
             <p>Account balance(saving): 1000</p>
             <p>Account balance(credit): 1000</p>
         </div>
-        <div class="row g-2">
-            <div class="col-6">
-                <div class="form-floating">
-                    <input type="number" class="form-control" placeholder="123456789" value="123456789">
-                    <label for="floatingInputGrid">Account number</label>
-                </div>
-            </div>
-        </div>
-            <button class="btn btn-primary" @click="editAccount(Account.id)">
-                Edit</button>*/
     </section>
 </template>
   
